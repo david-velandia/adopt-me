934d47100cd5df18d06fd75770605974cc2d846c
Andres Velandia <david.velandia@icloud.com>
2021-03-11 16:10:28 -0500

    component composition

---
+++ src/Results.js
@@ -0,0 +1,28 @@
+import Pet from './Pet';
+
+const Results = ({ pets }) => {
+    return (
+        <div className="search">
+            {
+                !pets.length ? (
+                    <h2>No Pets Found</h2>
+                ) : (
+                    pets.map((pet) => (
+                        <Pet
+                            animal={pet.animal}
+                            breed={pet.breed}
+                            id={pet.id}
+                            images={pet.images}
+                            key={pet.id}
+                            location={`${pet.city}, ${pet.state}`}
+                            name={pet.name}
+                        />
+                    ))
+                )
+            }
+        </div>
+    );
+}
+
+export default Results;
+
